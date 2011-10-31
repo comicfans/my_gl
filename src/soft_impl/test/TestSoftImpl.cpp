ed5d71239bb38815140ba8e9c446865155869988
comicfans <comicfans@comic.com>
2011-10-31 13:29:08 +0800

    add line clip test

--- src/soft_impl/test/TestSoftImpl.cpp
+++ src/soft_impl/test/TestSoftImpl.cpp
@@ -29,6 +29,8 @@ using namespace my_gl;
 
 
 	static const float  quodVertex[][2]={{0,2},{2,203},{204,204},{205,3}};
+     
+static const float directLines[][2]={{10,10},{10,200},{200,200},{200,10},{150,5}};
 
 	static const float  stripVertex[][2]={{5,2},{10,203},{205,16},{204,204}};
 
@@ -49,7 +51,6 @@ void testLines()
 
 void testSpecialLine()
 {
-     static const float directLines[][2]={{10,10},{10,300},{300,300},{300,10},{150,5}};
 
      context.vertexPointer(2, DataType::FLOAT, 0, directLines);
 
@@ -87,7 +88,26 @@ void testTriangles()
 
 	context.color4f(0.5, 0.5, 0.5, 0.5);
 
+	context.translatef(-30,0,0);
 	context.drawArrays(PrimitiveMode::TRIANGLE_STRIP,1,3);
+
+	context.flush();
+
+	SDL_Delay(2000);
+
+
+}
+
+void testLineClip()
+{
+
+     context.vertexPointer(2, DataType::FLOAT, 0, directLines);
+
+	context.color4f(0.5, 0.5, 0.5, 0.5);
+
+	context.translatef(0,-30,0);
+
+	context.drawArrays(PrimitiveMode::LINE_LOOP, 0, 5);
 
 	context.flush();
 
@@ -103,7 +123,9 @@ int main(int argc, char **argv)
 {
 
      init();
-     testSpecialLine();
+
+     testTriangles();
+
      return 0;
 }
 }
