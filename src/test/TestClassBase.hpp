cb7e0aa52e3112569c37b5291b8c0eef862dc015
comicfans <comicfans@comic.com>
2011-11-10 19:15:52 +0800

    add comment

--- src/test/TestClassBase.hpp
+++ src/test/TestClassBase.hpp
@@ -3,7 +3,16 @@
  *
  *       Filename:  TestClassBase.hpp
  *
- *    Description:  
+ *    Description:  this test class call the same 
+ *    OpenGL function for glut test and my_gl test
+ *    some GLES GL1.5 GL2.0 function use glew to 
+ *    call these function , but glew use macros to
+ *    replace gl call ,this leads function linkage
+ *    not same as gl.h define, and MyGL use exactly
+ *    the same linkage in gl.h , so these function 
+ *    can not be written in seperate implementation 
+ *    cpp file. these GL function call are defined 
+ *    depends on caller header definition
  *
  *        Version:  1.0
  *        Created:  2011-11-10 18:46:28
