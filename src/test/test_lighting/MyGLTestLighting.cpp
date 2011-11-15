a2b47595b59aae9e575a6e4cd0f3df9f05a58f58
comicfans <comicfans@comic.com>
2011-11-15 15:57:09 +0800

    use Tpl test

--- src/test/test_lighting/MyGLTestLighting.cpp
+++ src/test/test_lighting/MyGLTestLighting.cpp
@@ -15,26 +15,16 @@
  *
  * =====================================================================================
  */
-#define GL_GLEXT_PROTOTYPES
-#include <GL/gl.h>
-#include <GL/glext.h>
-#include "soft_impl/SoftContext.hpp"
+
+#include "MyGLTestTpl.hpp"
 #include "TestLighting.hpp"
 
 using namespace my_gl;
 
-SoftContext context(DEFAULT_WIDTH,DEFAULT_HEIGHT);
 
 
 int main(int argc, const char *argv[])
 {
-     TestLighting::init();
+     myGLTestRun<TestLighting>();
 
-     for (int i=0; i<300; ++i)
-     {
-	  TestLighting::render();
-	  glFlush();
-     }
-     
-	
 }
