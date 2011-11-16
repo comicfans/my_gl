c51afc57ccb0b23403067ad96deb7be981a91cf0
comicfans <comicfans@comic.com>
2011-11-16 18:21:15 +0800

    init glut by RGBA mode

--- src/test/GlutInit.cpp
+++ src/test/GlutInit.cpp
@@ -27,7 +27,7 @@ namespace my_gl {
 	  int argc=0;
      glutInit(&argc, nullptr);
 
-     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
 
      glutInitWindowSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
 
