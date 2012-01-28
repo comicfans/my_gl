ed97a8a881c3658e730a3ec15733b4f4795f01c1
comicfans <comicfans@comic.com>
2012-01-29 01:32:03 +0800

    add OpenCLFragmentAttributeBuffer ,expose rawData function to support OpenCL

---
+++ src/opencl_impl/shader/OpenCLFragmentAttributeBuffer.cpp
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  OpenCLFragmentAttributeBuffer.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012-1-28 20:25:33
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include "OpenCLFragmentAttributeBuffer.hpp"
+
+namespace my_gl {
+
+     OpenCLFragmentAttributeBuffer::OpenCLFragmentAttributeBuffer 
+	  (size_t width,size_t height,size_t attributeNumber)
+	  :FragmentAttributeBuffer(width,height,attributeNumber){}
+
+     const void * OpenCLFragmentAttributeBuffer::rawData()const
+     {
+	  return (*this)(0,0);
+     }
+
+
+} /* my_gl */
---
+++ src/opencl_impl/shader/OpenCLFragmentAttributeBuffer.hpp
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  OpenCLFragmentAttributeBuffer.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012-1-28 20:20:38
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef OPENCL_FRAGMENT_ATTRIBUTE_BUFFER_HPP
+
+#define OPENCL_FRAGMENT_ATTRIBUTE_BUFFER_HPP
+
+#include "soft_impl/shader/FragmentAttributeBuffer.hpp"
+
+namespace my_gl
+{
+     class OpenCLFragmentAttributeBuffer :public FragmentAttributeBuffer{
+     public:
+	  OpenCLFragmentAttributeBuffer 
+	     (size_t width,size_t height,size_t attributeNumber);
+
+	  virtual ~OpenCLFragmentAttributeBuffer ();
+
+	  const void* getRawData()const;
+     };
+	
+} /* my_gl */
+
+
+#endif /* end of include guard: OPENCL_FRAGMENT_ATTRIBUTE_BUFFER_HPP */
