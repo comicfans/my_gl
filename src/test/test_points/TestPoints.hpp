aff2619a884764c1ecc4522d015a30237eaa3357
comicfans <comicfans44@gmail.com>
2012-02-29 15:30:06 +0800

    tweak point generate

--- src/test/test_points/TestPoints.hpp
+++ src/test/test_points/TestPoints.hpp
@@ -20,15 +20,12 @@
 
 #define TEST_POINTS_HPP
 
-#include <cstdlib>
 
 #include "TestClassBase.hpp"
 
 
 
 namespace my_gl {
-
-     using std::rand;
 
 
      class TestPoints :public TestClassBase{
@@ -43,40 +40,80 @@ namespace my_gl {
 	  }
 
 	  static GLuint pointsVertexName;
+	  static GLuint pointsColorName;
+
+	  static int factor;
+
+	  static int pointNumber;
 
 	  static void createBuffer()
 	  {
 	       glEnableClientState(GL_VERTEX_ARRAY);
+	       glEnableClientState(GL_COLOR_ARRAY);
 
+	       factor=100;
+
+	       int yNumber=DEFAULT_HEIGHT/factor,
+		   xNumber=DEFAULT_WIDTH/factor;
+
+	       pointNumber=xNumber*yNumber;
+
+	       GLfloat gridPoints[pointNumber*2];
+
+	       GLfloat gridColors[pointNumber*3];
+
+	       for (int y=0; y<yNumber; ++y)
+	       {
+		    for (int x=0; x<xNumber; ++x)
+		    {
+			 int index=y*xNumber+x;
+
+			 gridPoints[index*2]=x*factor+50;
+			 gridPoints[index*2+1]=y*factor+50;
+
+			 gridColors[index*3]=float(index)/pointNumber;
+			 gridColors[index*3+1]=float(index)/pointNumber;
+			 gridColors[index*3+2]=float(index)/pointNumber;
+		    }
+		    
+	       }
+	
 	       glGenBuffers(1,&pointsVertexName);
 
 	       glBindBuffer(GL_ARRAY_BUFFER,pointsVertexName);
 
-	       GLfloat randomPoints[2000];
+	       glBufferData(GL_ARRAY_BUFFER,pointNumber*sizeof(float)*2,
+			 gridPoints,GL_STATIC_DRAW);
 
-	       for(int i=0;i<1000;++i)
-	       {
-		    randomPoints[i]=rand()%DEFAULT_WIDTH;
-		    randomPoints[i+1]=rand()%DEFAULT_HEIGHT;
-	       }
+	       glGenBuffers(1,&pointsColorName);
 
-	       glBufferData(GL_ARRAY_BUFFER,sizeof(randomPoints),
-			 randomPoints,GL_STATIC_DRAW);
+	       glBindBuffer(GL_ARRAY_BUFFER,pointsColorName);
+
+	       glBufferData(GL_ARRAY_BUFFER,pointNumber*sizeof(float)*3,
+			 gridColors,GL_STATIC_DRAW);
 
 	  }
 
 	  static void render()
 	  {
-	       glColor4f(1,0.2,0.1,1);
+
+	       glBindBuffer(GL_ARRAY_BUFFER,pointsVertexName);
 
 	       glVertexPointer(2,GL_FLOAT,0,0);
 
-	       glDrawArrays(GL_POINTS,0,1000);
+	       glBindBuffer(GL_ARRAY_BUFFER,pointsColorName);
+
+	       glColorPointer(3,GL_FLOAT,0,0);
+
+	       glDrawArrays(GL_POINTS,0,20);
 	  }
 
      };
 
      GLuint TestPoints::pointsVertexName;
+     GLuint TestPoints::pointsColorName;
+     int TestPoints::factor;
+     int TestPoints::pointNumber;
 	
 } /* my_gl */
 
